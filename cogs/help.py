70cd515fcfa38974aaaf4789bdbdf0e027f03442
DariuszTSulikowski03 <dariuszsulikowski.praca@gmail.com>
2025-02-06 00:54:08 +0100

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -1,4 +1,3 @@
-# cogs/help.py
 import discord
 from discord.ext import commands
 
@@ -6,27 +5,18 @@ class HelpCog(commands.Cog, name="Pomoc"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="help", help="Wyświetla listę dostępnych komend. Podaj nazwę komendy, aby uzyskać szczegóły.")
-    async def help(self, ctx, command_name: str = None):
-        if command_name:
-            cmd = self.bot.get_command(command_name)
-            if cmd:
-                embed = discord.Embed(
-                    title=f"Pomoc - {cmd.name}",
-                    description=cmd.help or "Brak dodatkowych informacji.",
-                    color=discord.Color.blue()
-                )
-                await ctx.send(embed=embed)
-            else:
-                await ctx.send("Nie znaleziono takiej komendy.")
-        else:
-            embed = discord.Embed(title="Lista komend", color=discord.Color.green())
-            for cog_name, cog in self.bot.cogs.items():
-                cmds = cog.get_commands()
-                if cmds:
-                    command_list = "\n".join([f"!{cmd.name} - {cmd.help}" for cmd in cmds if not cmd.hidden])
-                    embed.add_field(name=cog_name, value=command_list, inline=False)
-            await ctx.send(embed=embed)
+    @commands.command(name="help", help="Wyświetla listę dostępnych komend")
+    async def help(self, ctx):
+        embed = discord.Embed(title="Lista komend", color=discord.Color.green())
+        
+        for cog_name, cog in self.bot.cogs.items():
+            cmds = cog.get_commands()
+            if cmds:
+                command_list = "\n".join([f"**!{cmd.name}** - {cmd.help}" for cmd in cmds if not cmd.hidden])
+                embed.add_field(name=cog_name, value=command_list, inline=False)
+        
+        embed.set_footer(text="Wpisz !help [komenda] by zobaczyć szczegóły")
+        await ctx.send(embed=embed)
 
-def setup(bot):
-    bot.add_cog(HelpCog(bot))
+async def setup(bot):
+    await bot.add_cog(HelpCog(bot))
