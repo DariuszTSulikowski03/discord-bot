dec2f3674341a5f434abb0b9dcb7312e49a02714
DariuszTSulikowski03 <dariuszsulikowski.praca@gmail.com>
2025-02-06 00:06:56 +0100

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -6,13 +6,10 @@ class HelpCog(commands.Cog, name="Pomoc"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.hybrid_command(
-        name="help",
-        description="Wyświetla pomoc dotyczącą komend bota."
-    )
-    async def help(self, ctx, command: str = None):
-        if command:
-            cmd = self.bot.get_command(command)
+    @commands.command(name="help", help="Wyświetla listę dostępnych komend. Podaj nazwę komendy, aby uzyskać szczegóły.")
+    async def help(self, ctx, command_name: str = None):
+        if command_name:
+            cmd = self.bot.get_command(command_name)
             if cmd:
                 embed = discord.Embed(
                     title=f"Pomoc - {cmd.name}",
@@ -21,15 +18,15 @@ class HelpCog(commands.Cog, name="Pomoc"):
                 )
                 await ctx.send(embed=embed)
             else:
-                await ctx.send("Nie znaleziono komendy.")
+                await ctx.send("Nie znaleziono takiej komendy.")
         else:
-            embed = discord.Embed(title="Lista Komend", color=discord.Color.green())
+            embed = discord.Embed(title="Lista komend", color=discord.Color.green())
             for cog_name, cog in self.bot.cogs.items():
-                commands_list = "\n".join([f"!{cmd.name} - {cmd.help}" for cmd in cog.get_commands() if not cmd.hidden])
-                if commands_list:
-                    embed.add_field(name=cog_name, value=commands_list, inline=False)
+                cmds = cog.get_commands()
+                if cmds:
+                    command_list = "\n".join([f"!{cmd.name} - {cmd.help}" for cmd in cmds if not cmd.hidden])
+                    embed.add_field(name=cog_name, value=command_list, inline=False)
             await ctx.send(embed=embed)
 
-async def setup(bot):
-    await bot.add_cog(HelpCog(bot))
-
+def setup(bot):
+    bot.add_cog(HelpCog(bot))
